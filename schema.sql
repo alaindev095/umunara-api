473a83d5ea344c1600d7c2565db92a66b0d0fb04
alaindev095 <manzialain095@gmail.com>
2026-03-10 07:41:42 +0200

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 03, 2026 at 05:15 PM
+-- Generation Time: Mar 10, 2026 at 06:37 AM
 -- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- PHP Version: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,28 +18,8 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `manzi`
+-- Database: `umunara`
 --
-
--- --------------------------------------------------------
-
---
--- Table structure for table `books`
---
-
-CREATE TABLE `books` (
-  `id` int(11) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `price` decimal(10,2) NOT NULL,
-  `author` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `books`
---
-
-INSERT INTO `books` (`id`, `name`, `price`, `author`) VALUES
-(1, 'javascript', 13000.00, 'alain');
 
 -- --------------------------------------------------------
 
@@ -50,7 +30,7 @@ INSERT INTO `books` (`id`, `name`, `price`, `author`) VALUES
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
-  `email` varchar(150) NOT NULL,
+  `email` varchar(100) NOT NULL,
   `phone` varchar(20) NOT NULL,
   `password_hash` varchar(255) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
@@ -61,41 +41,29 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `phone`, `password_hash`, `created_at`) VALUES
-(1, 'Manzi', 'manzi@example.com', '0798901116', '$2b$10$DPQhtCeVjS4gNshbhivnNewXI6Cg0FEhKzJPmm4pKZ7tbZ4NcwyMG', '2026-03-03 15:18:37'),
-(2, 'alain', 'alain@gmail.com', '0798901110', '$2b$10$v55X0KmdT2HD1vB0qLwfz.KAY6knYxo9ToE1zkig6jIKM/uU0yku2', '2026-03-03 15:29:43');
+(1, 'manzi', 'manzi@gmail.com', '0798901116', '$2b$10$l6.3UgG0kL2RzGy4dbz3keB6RZSLwbN6/Ry8BloXTTYYirua2Oaka', '2026-03-10 05:30:09');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `books`
---
-ALTER TABLE `books`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`);
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `phone` (`phone`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `books`
---
-ALTER TABLE `books`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
